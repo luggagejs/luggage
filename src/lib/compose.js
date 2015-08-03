59dffe7a2b8edc0cf0336ebaa6e5317249abac76
dra1n <dra1n86@gmail.com>
2015-08-03 21:30:33 +0300

    Compose function.

---
+++ src/lib/compose.js
@@ -0,0 +1,11 @@
+export default function ComposeWithClass(...mixins) {
+  return (clazz) => {
+    const subclazz = class extends clazz {};
+
+    for (let mixin of mixins) {
+      Object.assign(subclazz.prototype, mixin);
+    }
+
+    return subclazz;
+  }
+}
