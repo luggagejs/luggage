c220e54e60919266864f5bc78729b6210a7d2d2b
dra1n <dra1n86@gmail.com>
2016-05-15 15:45:03 +0300

    Added new backend.

---
+++ src/backends/DropboxXMLHttpBackend.js
@@ -0,0 +1,83 @@
+const downloadApiPath = "https://content.dropboxapi.com/2/files/download";
+const uploadApiPath = "https://content.dropboxapi.com/2/files/upload";
+
+class DropboxCollection {
+  constructor(name, backend) {
+    this.name = name;
+    this.token = backend.token;
+  }
+
+  get fileName() {
+    return `${this.name}.json`;
+  }
+
+  read() {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+
+      request.onload = () => {
+        const data = JSON.parse(request.responseText);
+
+        if (data.error) {
+          if (data.error === "File not found") {
+            resolve([]);
+          } else {
+            reject(data.error);
+          }
+        } else {
+          resolve(data);
+        }
+      };
+
+      request.ontimeout = () => {
+        reject(request.responseText);
+      };
+      request.onerror = () => {
+        reject(request.responseText);
+      };
+      request.open("POST", downloadApiPath);
+      request.setRequestHeader("Authorization", `Bearer ${this.token}`);
+      request.setRequestHeader("Dropbox-API-Arg", JSON.stringify({ path: "/"+this.fileName }));
+      request.send();
+    });
+  }
+
+  write(data=[]) {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+
+      request.onload = () => {
+        const response = request.responseText;
+        if (response.error) {
+          reject(response.error);
+        } else {
+          resolve(data);
+        }
+      };
+
+      request.ontimeout = () => {
+        reject(request.responseText);
+      };
+      request.onerror = () => {
+        reject(request.responseText);
+      };
+      request.open("POST", uploadApiPath);
+      request.setRequestHeader("Authorization", `Bearer ${this.token}`);
+      request.setRequestHeader("Dropbox-API-Arg", JSON.stringify({ path: "/"+this.fileName }));
+      request.setRequestHeader("Content-Type", "text/plain; charset=dropbox-cors-hack");
+      request.send(JSON.stringify(data));
+    });
+  }
+}
+
+class DropboxBackend {
+  constructor(token) {
+    this.token = token;
+  }
+
+  collection(name) {
+    return new DropboxCollection(name, this);
+  }
+}
+
+export default DropboxBackend;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { default } from './Luggage';
+export { default as DropboxXMLHttpBackend } from './backends/DropboxXMLHttpBackend';
+export { default as DropboxBackend } from './backends/DropboxBackend';
+export { default as DropboxV1Backend } from './backends/DropboxV1Backend';
+export { default as DummyBackend } from './backends/DummyBackend';
