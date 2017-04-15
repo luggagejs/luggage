058345b3ec924b205b085dd24f7c5815b588a8f3
dra1n <dra1n86@gmail.com>
2017-04-15 12:14:39 +0300

    Export default dropbox backend

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default } from './Luggage';
+export { default as DropboxBackend } from './backends/DropboxBackend';
 export { default as DropboxXMLHttpBackend } from './backends/DropboxXMLHttpBackend';
 export { default as DropboxFetchBackend } from './backends/DropboxFetchBackend';
 export { default as DummyBackend } from './backends/DummyBackend';
