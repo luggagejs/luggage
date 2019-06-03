f4ac4c2e1e341182691d9a44b2b7a4209120d940
Anastasiia Bakuta <ms.sterh@gmail.com>
2019-06-03 13:12:55 +0300

    Use older syntax

--- packages/luggage/src/lib/compose.js
+++ packages/luggage/src/lib/compose.js
@@ -2,8 +2,8 @@ export default function ComposeWithClass(...mixins) {
   return (clazz) => {
     const subclazz = class extends clazz {}
 
-    for (let mixin of mixins) {
-      Object.assign(subclazz.prototype, mixin)
+    for (let i = 0; i < mixins.length; i++) {
+      Object.assign(subclazz.prototype, mixins[i])
     }
 
     return subclazz
